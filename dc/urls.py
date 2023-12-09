266390006811cc85fce1fa28a318ba9163b0a1a7
Melissa Longenberger <melissalongenberger24@gmail.com>
2023-12-09 02:27:57 -0500

    saving

---
+++ dc/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('orders/', include('order.urls')),
+    path('store/', include('store.urls')),
+]
